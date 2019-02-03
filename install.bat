9b0abebe7c8832503ef2bde58915d5b945863c02
Baldrickk <31375581+Baldrickk@users.noreply.github.com>
2019-02-03 19:02:59 +0000

    Modified installation - now uses symlinks.
    This does require that UAC permissions are acquired, so code to do that has been added too.
    The symlinks make for a much faster installation, and prevent the need for 5000+ copies of the same image file.

--- install.bat
+++ install.bat
@@ -4,14 +4,36 @@ SET install_dir=C:\Games\World_of_Tanks
 
 SET mod_dir=%install_dir%\res_mods\mods\shared_resources\xvm\res\clanicons\EU\
 
+net session >nul 2>&1
+    if %errorLevel% NEQ 0 (
+        goto UACPrompt
+    ) else (
+        goto gotAdmin
+    )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
 if exist %install_dir%\WorldOfTanks.exe (
-  rd /Q /S %mod_dir%\clan
+  rd /Q /S %mod_dir%\clan  >NUL
   md %mod_dir%\clan
-  rd /Q /S %mod_dir%\nick
+  rd /Q /S %mod_dir%\nick  >NUL
   md %mod_dir%\nick
   echo Installing forumite icons
-  for /F "tokens=*" %%N in (nicks.txt) do copy forumite_small.png %mod_dir%\nick\%%N.png >NUL
-  for /F "tokens=*" %%T in (trolls.txt) do copy warning_small.png %mod_dir%\nick\%%T.png >NUL
+  copy forumite_small.png %mod_dir%\nick\ >NUL
+  copy warning_small.png %mod_dir%\nick\ >NUL
+  for /F "tokens=*" %%N in (nicks.txt) do mklink %mod_dir%\nick\%%N.png %mod_dir%\nick\forumite_small.png >NUL 2>&1
+  for /F "tokens=*" %%T in (trolls.txt) do mklink %mod_dir%\nick\%%T.png %mod_dir%\nick\warning_small.png >NUL
   for /F "tokens=1,2 delims=," %%i in (clans.txt) do copy clanicons\%%j.png %mod_dir%\clan\%%i.png >NUL
   for /F "tokens=1,2 delims=," %%i in (special.txt) do copy special\%%j.png %mod_dir%\nick\%%i.png >NUL
 ) else (
